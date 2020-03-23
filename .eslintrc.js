bf776337e31f3901c2cf5a39c84d7f8a5d092490
Ygor <yrmuller@yahoo.com.br>
2020-03-22 22:50:59 -0300

    estilizando

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,31 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', 'prettier', 'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__:true
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks'],
   rules: {
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: "src"
+      },
+    },
   },
 };
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
   return (
-    <div />
+    <>
+    <img src={logo} alt="GoBarber" />
+
+    <form>
+      <input type="email" placeholder="Nome completo" />
+      <input type="password" placeholder="Seu e-mail" />
+      <input type="password" placeholder="Sua senha" />
+
+      <button type= "submit">Criar conta</button>
+      <Link to="/">Já tenho login</Link>
+    </form>
+    </>
   );
 }
+
