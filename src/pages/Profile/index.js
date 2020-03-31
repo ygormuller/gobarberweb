335701ada14addf9e1eb3bcac0e47a36f44f5641
Ygor <yrmuller@yahoo.com.br>
2020-03-31 16:54:01 -0300

    funcionalidades do perfil ok

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit (data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut(data) {
+    dispatch(signOut())
+  }
+
   return (
-    <div />
+  <Container>
+    <Form initialData={profile} onSubmit={handleSubmit}>
+      <AvatarInput name="avatar_id"/>
+
+      <Input name="name" placeholder="Nome completo" />
+      <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+      <hr />
+
+      <Input type="password" name="oldPassword" placeholder="Sua senha" />
+      <Input type="password" name="password" placeholder="Nova senha" />
+      <Input type="password" name="ConfirmPassword" placeholder="Confirmação de senha" />
+
+      <button type="submit">Atualizar perfil</button>
+    </Form>
+
+    <button type="button" onClick={handleSignOut}>Sair do GoBarber</button>
+  </Container>
   );
 }
